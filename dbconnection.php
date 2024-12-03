d9681d5634ee328e0e6f73a1874b2fca12f4a53f
Joevin <joevinansoc870@gmail.com>
2024-12-03 22:33:25 +0800

    sad

--- dbconnection.php
+++ dbconnection.php
@@ -4,7 +4,7 @@ class Database
     private $host = 'localhost';
     private $username = 'root';
     private $password = '';
-    private $dbname = 'hub_venue3';
+    private $dbname = 'icoco_rms';
     protected $connection = "";
 
     function connect()
