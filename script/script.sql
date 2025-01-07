c0a5da96ced27cfafb61a95554b511a3c50533ff
OwPor <rayvincentconcepcion@gmail.com>
2025-01-07 22:48:17 +0800

    events booking

---
+++ script/script.sql
@@ -0,0 +1,9 @@
+CREATE EVENT Book
+ON SCHEDULE EVERY 1 DATE
+STARTS '2025-01-07 00:00:01'
+ENDS '2026-01-07 00:00:01'
+ON COMPLETION NOT PRESERVE
+DO
+    UPDATE bookings 
+    SET booking_status_id = '4' 
+    WHERE booking_end_date < CURDATE();
